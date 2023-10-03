c6f82b9b8aaf7a49d4d857d8c4f16b3339d9fc6b
HDTran91 <khoaiga0103@gmail.com>
2023-10-03 03:49:38 -0500

    Register_validation_part1

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,18 @@
 import express from 'express'
 const usersRouter = express.Router()
-import { loginValidator } from '~/middlewares/users.middlewares'
+import { loginValidator, registerValidator } from '~/middlewares/users.middlewares'
 import { loginController, registerController } from '~/controllers/users.controller'
+import { validate } from '~/utils/validation'
 
 usersRouter.post('/login', loginValidator, loginController)
-usersRouter.post('/register', registerController)
+
+/**
+ * description: register a new user
+ * path" /register
+ * Method: POST
+ * body: {name: string, email: string, password:string,confirm_password: string,
+ * date_of_birth: ISO8601}
+ */
+usersRouter.post('/register', registerValidator, registerController)
 
 export default usersRouter
--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -2,7 +2,7 @@ import databaseService from '~/services/database.services'
 import User from '~/models/schemas/User.schema'
 
 class UsersService {
-  register(payload: { email: string, password: string }) {
+  register(payload: { email: string; password: string }) {
     const { email, password } = payload
     const result = databaseService.users.insertOne(new User({ email, password }))
     return result
