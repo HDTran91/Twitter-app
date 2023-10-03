cbd197b89de02f44c759aac82aa2703b072b1d0c
HDTran91 <khoaiga0103@gmail.com>
2023-10-03 00:10:22 -0500

    users_schema

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,9 @@
 import express from 'express'
 const usersRouter = express.Router()
 import { loginValidator } from '~/middlewares/users.middlewares'
-import { loginController } from '~/controllers/users.controller'
+import { loginController, registerController } from '~/controllers/users.controller'
 
 usersRouter.post('/login', loginValidator, loginController)
+usersRouter.post('/register', registerController)
 
 export default usersRouter
--- src/services/database.services.ts
+++ src/services/database.services.ts
@@ -1,5 +1,6 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient, Db, Collection } from 'mongodb'
 import { config } from 'dotenv'
+import User from '~/models/schemas/User.schema'
 config()
 console.log(process.env.DB_USERNAME)
 
@@ -7,19 +8,24 @@ const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}
 
 class DatabaseService {
   private client: MongoClient
+  private db: Db
   constructor() {
     // Create a MongoClient with a MongoClientOptions object to set the Stable API version
     this.client = new MongoClient(uri)
+    this.db = this.client.db(process.env.DB_NAME)
   }
   async connect() {
     try {
       // Send a ping to confirm a successful connection
-      await this.client.db('admin').command({ ping: 1 })
+      await this.db.command({ ping: 1 })
       console.log('Pinged your deployment. You successfully connected to MongoDB!')
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await this.client.close()
+    } catch (error) {
+      console.log('Error', error)
+      throw error
     }
+  }
+  get users(): Collection<User> {
+    return this.db.collection(process.env.DB_USERS_COLLECTION as string)
   }
 }
 
