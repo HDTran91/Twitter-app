8a8edcd3c5a704a1174da2488614d4ff16924d24
HDTran91 <khoaiga0103@gmail.com>
2023-12-06 23:18:29 -0600

    create file upload when running server

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,15 @@ import userRouter from '~/routes/users.routes'
 import databaseService from '~/services/database.services'
 import { defaultErrorHandler } from './middlewares/error.middlewares'
 import mediasRouter from './routes/medias.routes'
+import { initFolder } from './utils/file'
 
 //connect to database
 databaseService.connect()
 const app = express()
 const port = 4000
+
+//create folder upload
+initFolder()
 
 app.use(express.json())
 app.use('/users', userRouter)
