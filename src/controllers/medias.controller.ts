9d6311a29f131cad335048e50191d1e1ef27e7bf
HDTran91 <khoaiga0103@gmail.com>
2023-12-06 22:20:09 -0600

    Upload file

---
+++ src/controllers/medias.controller.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from 'express'
+import path from 'path'
+
+console.log(path.resolve('uploads'))
+export const uploadSingleImageController = async (req: Request, res: Response, next: NextFunction) => {
+  const formidable = (await import('formidable')).default
+  const form = formidable({
+    uploadDir: path.resolve('uploads'),
+    maxFiles: 1,
+    keepExtensions: true,
+    maxFileSize: 300 * 1024 //300KB
+  })
+  //check err or not
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      throw err
+    }
+    res.json({
+      message: 'Upload image successfully'
+    })
+  })
+}
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import userRouter from '~/routes/users.routes'
 import databaseService from '~/services/database.services'
 import { defaultErrorHandler } from './middlewares/error.middlewares'
+import mediasRouter from './routes/medias.routes'
 
 //connect to database
 databaseService.connect()
@@ -10,6 +11,7 @@ const port = 4000
 
 app.use(express.json())
 app.use('/users', userRouter)
+app.use('/medias', mediasRouter)
 
 //error handler
 
