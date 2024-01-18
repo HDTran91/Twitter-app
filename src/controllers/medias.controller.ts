cc6f8f8e3068aedabe4fbfa12bc13c80239e7e87
HDTran91 <khoaiga0103@gmail.com>
2024-01-18 13:55:10 -0600

    another way of serving static image

--- src/controllers/medias.controller.ts
+++ src/controllers/medias.controller.ts
@@ -1,7 +1,8 @@
 import { NextFunction, Request, Response } from 'express'
+import path from 'path'
+import { UPLOAD_DIR } from '~/constants/dir'
 import { USERS_MESSAGES } from '~/constants/messages'
 import mediasService from '~/services/medias.services'
-
 
 // console.log(path.resolve('uploads'))
 export const uploadSingleImageController = async (req: Request, res: Response, next: NextFunction) => {
@@ -9,5 +10,15 @@ export const uploadSingleImageController = async (req: Request, res: Response, n
   return res.json({
     message: USERS_MESSAGES.UPLOAD_SUCCESS,
     result: url
+  })
+}
+
+export const serveImageController = async (req: Request, res: Response, next: NextFunction) => {
+  const { name } = req.params
+  console.log(name)
+  return res.sendFile(path.resolve(UPLOAD_DIR, name), (err) => {
+    if (err) {
+      res.status((err as any).status).send('Not Found')
+    }
   })
 }
